76a0db3836b41d3a361569aa7243c087434685ee
samibaou <sami.baou@uha.fr>
2025-06-09 15:06:57 +0200

    Ajout de la 2eme version partie front-end

---
+++ front-end/app/src/main/java/com/example/here_presence_app/core/navigation/PresenceApp.kt
@@ -0,0 +1,34 @@
+package com.example.here_presence_app.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.here_presence_app.presentation.ui.splash.SplashScreen
+import com.example.here_presence_app.presentation.ui.login.LoginScreen
+//import com.example.here_presence_app.presentation.ui.professor.ProfHomeScreen
+import com.example.here_presence_app.presentation.ui.student.main.StudentScreen
+
+@Composable
+fun PresenceApp() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+        composable("splash") {
+            SplashScreen(navController)
+        }
+        composable("login") {
+            LoginScreen(navController)
+        }
+        //composable("prof_home") {
+           // TeacherDashboardFragment(navController)
+        //}
+        composable("student_home") {
+            StudentScreen()
+        }
+    }
+}
